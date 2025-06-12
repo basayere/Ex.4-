e1be35239bba8d6fb0675db5473b03de0187907f
Basayer <136784542+basayere@users.noreply.github.com>
2025-06-12 15:19:59 +0700

    Add files via upload

---
+++ BandC7.4.py
@@ -0,0 +1,5 @@
+for i in range(1, 11):
+    if 1 % 2 != 0:
+        continue
+    print(i)
+    
---
+++ BandC7.5.py
@@ -0,0 +1,10 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for row in matrix:
+    for val in row:
+        if val == 5:
+            continue  
+        if val == 8:
+            break 
+        print(val, end=" ")
+    print() 
